409cecc0bafe29a468f8aa4ff2b1e776bf5f1e36
BilinSun02 <BilinSun02@outlook.com>
2026-09-05 10:16:04 +0000

    Manual upload

--- kext/MBUnthrottleUserClient.hpp
+++ kext/MBUnthrottleUserClient.hpp
@@ -2,32 +2,57 @@
 #pragma once
 
 #include <IOKit/IOUserClient.h>
+
 #include "../include/MBUProtocol.h"
 
 class MBUnthrottleService;
 
-class MBUnthrottleUserClient : public IOUserClient {
-    OSDeclareDefaultStructors(MBUnthrottleUserClient)
+class MBUnthrottleUserClient :
+    public IOUserClient {
+
+    OSDeclareDefaultStructors(
+        MBUnthrottleUserClient)
 
 public:
-    bool initWithTask(task_t owningTask,
-                      void *securityID,
-                      UInt32 type,
-                      OSDictionary *properties) override;
-    bool start(IOService *provider) override;
-    IOReturn clientClose() override;
 
-    IOReturn externalMethod(uint32_t selector,
-                            IOExternalMethodArguments *arguments,
-                            IOExternalMethodDispatch *dispatch = nullptr,
-                            OSObject *target = nullptr,
-                            void *reference = nullptr) override;
+    bool initWithTask(
+        task_t owningTask,
+        void *securityID,
+        UInt32 type,
+        OSDictionary *properties)
+        override;
+
+    bool start(
+        IOService *provider)
+        override;
+
+    IOReturn clientClose()
+        override;
+
+    IOReturn externalMethod(
+        uint32_t selector,
+        IOExternalMethodArguments *arguments,
+        IOExternalMethodDispatch *dispatch = nullptr,
+        OSObject *target = nullptr,
+        void *reference = nullptr)
+        override;
 
 private:
-    MBUnthrottleService *owner_ = nullptr;
-    static const IOExternalMethodDispatch methods_[kMBUSelectorCount];
 
-    static IOReturn sGetStatus(OSObject *target,
-                               void *reference,
-                               IOExternalMethodArguments *arguments);
+    MBUnthrottleService *owner_ =
+        nullptr;
+
+    static const
+    IOExternalMethodDispatch
+    methods_[kMBUSelectorCount];
+
+    static IOReturn sGetStatus(
+        OSObject *target,
+        void *reference,
+        IOExternalMethodArguments *arguments);
+
+    static IOReturn sRestoreDefaults(
+        OSObject *target,
+        void *reference,
+        IOExternalMethodArguments *arguments);
 };
