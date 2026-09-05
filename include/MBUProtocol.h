83ef7875f426671a0b9bd3e595e0455895209699
BilinSun02 <BilinSun02@outlook.com>
2026-09-05 10:14:47 +0000

    Manual upload

--- include/MBUProtocol.h
+++ include/MBUProtocol.h
@@ -16,19 +16,22 @@ enum MBUCluster : uint32_t {
 
 enum MBUSelector : uint32_t {
     kMBUSelectorGetStatus = 0,
-    kMBUSelectorCount = 1,
+    kMBUSelectorRestoreDefaults = 1,
+    kMBUSelectorCount = 2,
 };
 
 struct MBUClusterStatus {
     uint64_t cluster_base;
     uint64_t command_phys;
+
     uint64_t raw_command;
     uint64_t raw_status;
     uint64_t last_change;
     uint64_t pll_status;
     uint64_t pll_factor;
+
     uint32_t requested_pstate;
-    uint32_t reserved;
+    uint32_t default_pstate;
 };
 
 struct MBUStatusReply {
@@ -37,4 +40,4 @@ struct MBUStatusReply {
     MBUClusterStatus clusters[kMBUClusterCount];
 };
 
-static constexpr uint32_t kMBUProtocolVersion = 1;
+static constexpr uint32_t kMBUProtocolVersion = 2;
