a3e2069eac6939e07da66b81dfc2b8c487a2a1b3
BilinSun02 <BilinSun02@outlook.com>
2026-09-05 05:00:22 -0500

    Make service interface read-only

--- kext/MBUnthrottleService.hpp
+++ kext/MBUnthrottleService.hpp
@@ -23,14 +23,11 @@ public:
                            IOUserClient **handler) override;
 
     IOReturn copyStatus(MBUStatusReply *reply);
-    IOReturn setPState(uint32_t cluster, uint32_t pstate);
-    IOReturn restoreDefaults();
 
 private:
     struct ClusterMap {
         const char *name;
         uint64_t clusterBase;
-        uint32_t defaultPState;
         IOMemoryDescriptor *descriptor;
         IOMemoryMap *mapping;
         volatile uint8_t *regs;
@@ -44,24 +41,16 @@ private:
     static constexpr uint32_t kStatusOffset = 0x50;
     static constexpr uint32_t kPLLStatusOffset = 0xc0;
     static constexpr uint32_t kPLLFactorOffset = 0xc8;
-
-    static constexpr uint64_t kCmdBusy = (1ULL << 31);
-    static constexpr uint64_t kCmdSet = (1ULL << 25);
     static constexpr uint64_t kCmdPStateMask = 0x1fULL;
 
-    // m1n1 src/cpufreq.c, t6020_clusters[]
     ClusterMap clusters_[kMBUClusterCount] = {
-        {"ECPU0", 0x210e00000ULL, 5, nullptr, nullptr, nullptr},
-        {"PCPU0", 0x211e00000ULL, 6, nullptr, nullptr, nullptr},
-        {"PCPU1", 0x212e00000ULL, 6, nullptr, nullptr, nullptr},
+        {"ECPU0", 0x210e00000ULL, nullptr, nullptr, nullptr},
+        {"PCPU0", 0x211e00000ULL, nullptr, nullptr, nullptr},
+        {"PCPU1", 0x212e00000ULL, nullptr, nullptr, nullptr},
     };
 
     bool runningOnT6020();
     bool mapCluster(ClusterMap &cluster);
     void unmapCluster(ClusterMap &cluster);
-
     uint64_t read64(const ClusterMap &cluster, uint32_t offset) const;
-    void write64(const ClusterMap &cluster, uint32_t offset, uint64_t value) const;
-
-    IOReturn setPStateLocked(ClusterMap &cluster, uint32_t pstate);
 };
