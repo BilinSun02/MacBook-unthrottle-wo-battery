9b1096524c6506f7ec6cdd4e0dbd20ef781c0f2b
BilinSun02 <BilinSun02@outlook.com>
2026-09-05 10:15:16 +0000

    Manual upload

--- kext/MBUnthrottleService.hpp
+++ kext/MBUnthrottleService.hpp
@@ -4,6 +4,7 @@
 #include <IOKit/IOService.h>
 #include <IOKit/IOMemoryDescriptor.h>
 #include <IOKit/IOMemoryMap.h>
+
 #include "../include/MBUProtocol.h"
 
 class MBUnthrottleUserClient;
@@ -23,34 +24,97 @@ public:
                            IOUserClient **handler) override;
 
     IOReturn copyStatus(MBUStatusReply *reply);
+    IOReturn restoreDefaults();
 
 private:
     struct ClusterMap {
         const char *name;
         uint64_t clusterBase;
+        uint32_t defaultPState;
+
         IOMemoryDescriptor *descriptor;
         IOMemoryMap *mapping;
         volatile uint8_t *regs;
     };
 
+    /*
+     * T6020 cluster P-state block.
+     *
+     * m1n1 uses:
+     *
+     *     cluster base + 0x20020
+     *
+     * We map the containing 4K page.
+     */
     static constexpr uint64_t kDVFSPageOffset = 0x20000;
     static constexpr uint64_t kDVFSPageLength = 0x1000;
 
-    static constexpr uint32_t kCmdOffset = 0x20;
+    static constexpr uint32_t kCmdOffset        = 0x20;
     static constexpr uint32_t kLastChangeOffset = 0x38;
-    static constexpr uint32_t kStatusOffset = 0x50;
-    static constexpr uint32_t kPLLStatusOffset = 0xc0;
-    static constexpr uint32_t kPLLFactorOffset = 0xc8;
-    static constexpr uint64_t kCmdPStateMask = 0x1fULL;
+    static constexpr uint32_t kStatusOffset     = 0x50;
+    static constexpr uint32_t kPLLStatusOffset  = 0xc0;
+    static constexpr uint32_t kPLLFactorOffset  = 0xc8;
 
+    /*
+     * Reverse engineered by Asahi:
+     *
+     * bit 31   busy
+     * bit 25   submit/set
+     * bits 4:0 desired P-state
+     */
+    static constexpr uint64_t kCmdBusy =
+        1ULL << 31;
+
+    static constexpr uint64_t kCmdSet =
+        1ULL << 25;
+
+    static constexpr uint64_t kCmdPStateMask =
+        0x1fULL;
+
+    /*
+     * T6020 values from m1n1/src/cpufreq.c.
+     *
+     * These are m1n1's NORMAL DEFAULT states, not assumed boost/max
+     * states.
+     */
     ClusterMap clusters_[kMBUClusterCount] = {
-        {"ECPU0", 0x210e00000ULL, nullptr, nullptr, nullptr},
-        {"PCPU0", 0x211e00000ULL, nullptr, nullptr, nullptr},
-        {"PCPU1", 0x212e00000ULL, nullptr, nullptr, nullptr},
+        {
+            "ECPU0",
+            0x210e00000ULL,
+            5,
+            nullptr,
+            nullptr,
+            nullptr
+        },
+        {
+            "PCPU0",
+            0x211e00000ULL,
+            6,
+            nullptr,
+            nullptr,
+            nullptr
+        },
+        {
+            "PCPU1",
+            0x212e00000ULL,
+            6,
+            nullptr,
+            nullptr,
+            nullptr
+        },
     };
 
     bool runningOnT6020();
+
     bool mapCluster(ClusterMap &cluster);
     void unmapCluster(ClusterMap &cluster);
-    uint64_t read64(const ClusterMap &cluster, uint32_t offset) const;
+
+    uint64_t read64(const ClusterMap &cluster,
+                    uint32_t offset) const;
+
+    void write64(const ClusterMap &cluster,
+                 uint32_t offset,
+                 uint64_t value) const;
+
+    IOReturn setKnownDefaultPState(ClusterMap &cluster);
 };
